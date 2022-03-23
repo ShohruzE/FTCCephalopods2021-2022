477adb5481c85207d3a7a12b2ba3f623f3476754
ShohruzE <shohruz123456789@gmail.com>
2022-03-22 22:25:08 -0400

    Fixed subsystem hardwareMap error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystemtest/SusbystemTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystemtest/SusbystemTeleOp.java
@@ -1,39 +1,43 @@
-package org.firstinspires.ftc.teamcode.FTC_8088;
+package org.firstinspires.ftc.teamcode.subsystemtest;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystemtest.Arm;
-import org.firstinspires.ftc.teamcode.subsystemtest.Capper;
-import org.firstinspires.ftc.teamcode.subsystemtest.Drive;
-import org.firstinspires.ftc.teamcode.subsystemtest.Intake;
-import org.firstinspires.ftc.teamcode.subsystemtest.Turret;
-
-
 @TeleOp
-public class SubsystemTeleOp extends OpMode {
+public class SusbystemTeleOp extends OpMode {
+
+    /*
     Arm arm = new Arm(this);
     Capper capper = new Capper(this);
-    Drive drive = new Drive(this);
     Intake intake = new Intake(this);
     Turret turret = new Turret(this);
+    */
+    Drive drive = new Drive(this);
 
 
     @Override
     public void init() {
+
+        /*
         arm.init();
         capper.init();
-        drive.init();
         intake.init();
         turret.init();
+        */
+        drive.init();
+
+
     }
 
     @Override
     public void loop() {
+
+        /*
         arm.run();
         capper.run();
-        drive.run();
         intake.run();
         turret.run();
+        */
+        drive.run();
     }
 }
